2b94eebd5a15a0e8c3e7071b449608a766463b5c
Hedgy134117 <hedgy134117@gmail.com>
2020-12-16 15:05:06 -0500

    fix CORS url

--- terminet/terminet/settings.py
+++ terminet/terminet/settings.py
@@ -135,6 +135,6 @@ STATIC_URL = '/static/'
 # CORS
 CORS_ALLOWED_ORIGINS = [
     'http://127.0.0.1:5500',
-    'https://hedgy134117.github.io/',
+    'https://hedgy134117.github.io',
 ]
 CORS_ALLOW_CREDENTIALS = True
